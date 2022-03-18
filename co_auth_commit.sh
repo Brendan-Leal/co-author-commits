1f55932db09ad00f7920840668fa250ee6f9c354
Brendan Leal <brendan.leal@outlook.com>
2022-03-17 22:45:22 -0700

    fix issue where commit messages show an extra " charater
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
    Co-authored-by: Drew Sessler <drew.sessler@gmail.com>
    Co-authored-by: Brendan Leal <leal.brendan@gmail.com>"

--- co_auth_commit.sh
+++ co_auth_commit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 read -p "Enter commit message: " r1
-echo git commit -m "$r1
+git commit -m "$r1
 
 
 Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
 Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
 Co-authored-by: Drew Sessler <drew.sessler@gmail.com>
-Co-authored-by: Brendan Leal <leal.brendan@gmail.com>\"" 
+Co-authored-by: Brendan Leal <leal.brendan@gmail.com>\" 

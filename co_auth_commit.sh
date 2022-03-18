1adb66a235ad66f5939949b47942b1b1da1c8c67
Brendan Leal <brendan.leal@outlook.com>
2022-03-17 23:01:09 -0700

    final fix?
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
    Co-authored-by: Drew Sessler <drew.sessler@gmail.com>
    Co-authored-by: Brendan Leal <leal.brendan@gmail.com>

--- co_auth_commit.sh
+++ co_auth_commit.sh
@@ -7,4 +7,4 @@ git commit -m "$r1
 Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
 Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
 Co-authored-by: Drew Sessler <drew.sessler@gmail.com>
-Co-authored-by: Brendan Leal <leal.brendan@gmail.com>\"
+Co-authored-by: Brendan Leal <leal.brendan@gmail.com>"
